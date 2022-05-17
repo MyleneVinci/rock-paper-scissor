c017616725b07b833261c0b908a42ec37bee0078
Mylene Vinci <mylene.vinci@hotmail.com>
2022-05-17 20:45:03 +0200

    clean clode

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result p");
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissors_div = document.getElementById("scissors");
+const userScoreDisplay = document.getElementById("user-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+const scoreBoardDisplay = document.querySelector(".score-board");
+const resultDisplay = document.querySelector(".result p");
+const rockDisplay = document.getElementById("rock");
+const paperDisplay = document.getElementById("paper");
+const scissorsDisplay = document.getElementById("scissors");
+const lizardDisplay = document.getElementById("lizard");
+const spockDisplay = document.getElementById("spock");
+const resultMessage = document.getElementById("result-message");
+const tryAgainMessage = document.getElementById("play-message");
+const choice = document.querySelector(".choice");
+
 
 const convertToFrench = (word) => {
         if ( word === "rock") return "Pierre";
@@ -20,41 +26,50 @@ const getComputerChoice = () => {
     return choices[randomNumber];
 }
 
+const gameResult =(result) => {  
+    resultMessage.innerHTML = `${result} !`;
+    tryAgainMessage.innerHTML = " ";
+    setTimeout(() => {tryAgainMessage.innerHTML = `Joue encore !`}, 800);
+}
+
 
 const win = (userChoice, computerChoice) => {
     userScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_div.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}. Tu as gagné !`
+    userScoreDisplay.innerHTML = userScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    resultDisplay.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}`
     document.getElementById(userChoice).classList.add('green-glow');
     setTimeout(() => { document.getElementById(userChoice).classList.remove('green-glow')}, 300);
     console.log('win');
+    gameResult("gagné");
 }
 
 const lose = (userChoice, computerChoice) => {
     computerScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_div.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}. Tu as perdu !`
+    userScoreDisplay.innerHTML = userScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    resultDisplay.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}`
     document.getElementById(userChoice).classList.add('red-glow');
     setTimeout(() => { document.getElementById(userChoice).classList.remove('red-glow')}, 300);
     console.log('lose');
+    gameResult("perdu");
 }
 
 const draw = (userChoice, computerChoice) => {
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_div.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}. Egalité !`
+    userScoreDisplay.innerHTML = userScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    resultDisplay.innerHTML = `Tu as choisi ${convertToFrench(userChoice)}, ton adversaire a choisi ${convertToFrench(computerChoice)}`
     document.getElementById(userChoice).classList.add('grey-glow');
     setTimeout(() => { document.getElementById(userChoice).classList.remove('grey-glow')}, 300);
     console.log('draw');
+    gameResult("Egalité");
 }
 
 
 const game = (userChoice) => {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
-        case "rockscissors":
+        case "rockscissors": 
         case "paperrock":
         case "scissorspaper":
             win(userChoice, computerChoice);
@@ -73,9 +88,9 @@ const game = (userChoice) => {
 }
 
 const main = () => {
-    rock_div.addEventListener('click', () => game("rock"));
-    paper_div.addEventListener('click', () => game("paper"));
-    scissors_div.addEventListener('click', () => game("scissors"));
+    rockDisplay.addEventListener('click', () => game("rock"));
+    paperDisplay.addEventListener('click', () => game("paper"));
+    scissorsDisplay.addEventListener('click', () => game("scissors"));
 }
 main();
 
